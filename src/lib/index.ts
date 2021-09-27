4fece7e456151efd9921aadeeb3152a409d61ef8
Lady Sun <ladysun.freedom@gmail.com>
2021-09-27 20:06:22 +0200

    main > better export in index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,20 @@
-export * as hooks from "./hooks";
+export { useAddEvent, useDisplay } from "./hooks";
 export * as i18n from "./i18n";
-export * as model from "./model";
+export {
+  LMCart,
+  LMCartProduct,
+  LMColor,
+  LMSize,
+  LMProduct,
+  LMProductProps,
+  LMPromo,
+  LMPromoType,
+  LMAddress,
+  LMComment,
+  LMCreditCard,
+  LMImg,
+  LMMeasures,
+  LMPropsBuy,
+  LMUser,
+  LMUserInfo,
+} from "./model";
