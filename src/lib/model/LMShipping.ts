86b36b325bc1c0dfd571fa071b783fdfdd708e68
Lady Sun <ladysun.freedom@gmail.com>
2021-10-01 12:53:34 +0200

    main > added LM Shipping

---
+++ src/lib/model/LMShipping.ts
@@ -0,0 +1,10 @@
+type LMShipping = {
+  id: string;
+  initDate: Date;
+  finishDate: Date;
+  priceShipping: Date;
+  shippingMethod: string;
+  paymentMethod: string;
+};
+
+export { LMShipping };
