b816a224ea1c9a7e35564265bd7b14053a584e1c
Lady Sun <ladysun.freedom@gmail.com>
2021-10-05 00:24:13 +0200

    main > fixed _id CartProduct

--- src/lib/model/LMCart/LMCartProduct.ts
+++ src/lib/model/LMCart/LMCartProduct.ts
@@ -2,7 +2,7 @@ import { LMSize, LMColor } from "../LMEnum";
 import { LMProduct } from "../LMProduct";
 
 type LMCartProduct = {
-  id: string;
+  _id?: string;
   size: LMSize;
   color?: LMColor;
   unds: number;
