448e0fd91a5038765d0a5eda9094a736f7ed77a4
Kodeneko <ladysun.freedom@gmail.com>
2023-03-31 20:15:42 +0200

    [dist] eslint config jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,12 +4,7 @@ const config: Config.InitialOptions = {
     testPathIgnorePatterns: ['node_modules', '<rootDir>/dist', "<rootDir>/build"],
     transform: {
         "\.(ts|tsx|jsx)$": "ts-jest"
-    },
-    moduleNameMapper: {
-        "\.module.(scss|sass)": "identity-obj-proxy",
-        "\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `jest-transform-stub`
-    },
-    testEnvironment: 'jsdom'
+    }
 }
 
 export default config
